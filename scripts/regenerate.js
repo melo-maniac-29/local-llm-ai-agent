4220a0b006bae37eca4e6369097cfb46de31d3cb
melo-maniac-29 <allenbobby2003@gmail.com>
2025-05-24 20:29:41 +0530

    done

--- scripts/regenerate.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-console.log('Regenerating Convex API client...');
-try {
-  execSync('npx convex dev --once', { stdio: 'inherit' });
-  console.log('API client regenerated successfully!');
-} catch (error) {
-  console.error('Error regenerating API client:', error);
-}
